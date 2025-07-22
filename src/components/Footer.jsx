6aa3a72a5b203c648a53272f04ee435d8a5b9d1d
Arsn1323 <ankitrajvr386@gmail.com>
2025-07-22 18:45:03 +0530

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='max-w-[1300px] mx-auto bg-black flex justify-between p-6 md:p-20 text-sm md:text-lg mt-12'>
         <div className='space-y-4'>
-            <h3 className='text-2xl text-gray-200 font-semibold'>Made with by ANKIT RAJ</h3>
+            <h3 className='text-2xl text-gray-200 font-semibold'>© 2025 Ankit Raj.</h3>
             <div className='flex flex-row gap-6 text-gray-400 text-4xl'>
                 <a href="https://github.com/Arsn1323"><FaGithubSquare /></a>
                 <a href="https://www.instagram.com/ankitrajvr386/profilecard/?igsh=b215YXMzajd3bm1h"><FaInstagram /></a>
